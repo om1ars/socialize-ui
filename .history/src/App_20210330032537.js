cb2c324ae415d6fb4ce28353b7ed5846eedf3c95
omar-slf <nurullayevumarali1@gmail.com>
2021-11-24 22:25:40 +0500

    co cimmit

--- .history/src/App_20210330032537.js
+++ .history/src/App_20210330032537.js
@@ -2,7 +2,6 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-import './App.css'
 
 function App() {
   return <Profile/>;
